54031036092fc9815b25a05734a87ac15d25419c
Alexander Gliedman-Adler <alexgliedmanadler@gmail.com>
2015-05-23 21:29:15 -0400

    Initial Commit
    Initial project template commit

---
+++ NerdDinner1.4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NerdDinner1._4.Startup))]
+namespace NerdDinner1._4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
