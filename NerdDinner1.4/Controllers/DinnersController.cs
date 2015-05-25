bfa20703e4c168c5f069a816b3d56d0600661e0f
Alexander Gliedman-Adler <alexgliedmanadler@gmail.com>
2015-05-25 03:17:54 -0400

    NerdDinnerContext > NerdDinnerDataContext
    Using the MVC4 nerddinner has us using a NerdDinnerContext as opposed to
    the NerdDinnerDataContext. Updated accordingly, there is some legacy
    cruff left to be removed but I'm not sure what's unused just yet.

--- NerdDinner1.4/Controllers/DinnersController.cs
+++ NerdDinner1.4/Controllers/DinnersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 using NerdDinner1._4.Models;
 using PagedList;
 
@@ -13,28 +14,54 @@ namespace NerdDinner1._4.Controllers
     {
         // dinnerRepository global
         DinnerRepository dinnerRepository = new DinnerRepository();
-        private NerdDinnerDataContext db = new NerdDinnerDataContext(); 
+        private NerdDinnerContext db = new NerdDinnerContext(); 
         //
         // GET: /Dinners/
 
+        private const int pageSize = 25;
         public ActionResult Index(int? page)
         {
             int pageIndex = page ?? 1;
             //var dinners = dinnerRepository.FindUpcomingDinners().ToList();
             var dinners = db.Dinners.OrderBy(d => d.EventDate);
-            return View(dinners.ToPagedList(pageIndex, 25));
-            //return View("Index", dinners);
+            return View(dinners.ToPagedList(pageIndex, pageSize));
         }
         //
         // GET: /Dinners/Details/2
 
-        public ActionResult Details(int id)
+        public ActionResult Details(int id = 0)
+        {
+            Dinner dinner = db.Dinners.Find(id);
+            if (dinner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dinner);
+        }
+
+        //
+        // GET: /Dinners/Edit/2
+        public ActionResult Edit(int id)
         {
             Dinner dinner = dinnerRepository.GetDinner(id);
-            if (dinner == null)
-                return View("NotFound");
-            else
-                return View(dinner);
+            return View(dinner);
         }
+
+        //
+        // POST: /Dinners/Edit/2
+
+        [HttpPost]
+        public ActionResult Edit(Dinner dinner)
+        {
+            /*if(!dinner.IsHostedBy(User.Identity.Name)) {
+                return View("InvalidOwner");
+            }*/
+            if(ModelState.IsValid) 
+            {
+                db.Entry(dinner).State = EntityState.Modified;
+            }
+            return View(dinner);
+        }
+    
     }
 }
